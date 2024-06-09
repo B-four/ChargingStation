a2b50d45d34de8d19d62ee5630388f0d4124a74a
g_ung <rldnd0705@gmail.com>
2024-06-09 20:39:16 +0900

    bookmark function modify

--- src/main/java/com/example/chargingstation/usercontrol/repository/BookmarkRepository.java
+++ src/main/java/com/example/chargingstation/usercontrol/repository/BookmarkRepository.java
@@ -4,9 +4,13 @@ import com.example.chargingstation.stationcontrol.entity.Station;
 import com.example.chargingstation.usercontrol.entity.Bookmark;
 import com.example.chargingstation.usercontrol.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     Optional<Bookmark> findByUserAndStation(User user, Station station);
+    List<Bookmark> findByUser(User user);
 }
